3e71787a32d127a55a7b385f6d1bf15d3938a5e0
Ahmed R. Mazen <ahmed.mazen87@yahoo.com>
2022-08-13 23:15:57 +0300

    create the project

---
+++ src/main/java/io/eduacative/mediaapp/MediaappApplication.java
@@ -0,0 +1,13 @@
+package io.eduacative.mediaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediaappApplication.class, args);
+    }
+
+}
